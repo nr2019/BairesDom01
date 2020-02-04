fad5e061c594d3d7ad5dc80b75d1facab5342e0f
nr2019 <nahuel.ryzy@gmail.com>
2020-02-04 08:53:44 -0300

    20200104 090000

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,15 +11,37 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
   @Input() navitems: InavMi;
   @Input() valor: boolean;
-
+  @Input() seccion: string;
+  cssSeccion = [];
   constructor(private actionSheetCtrl: ActionSheetController,
               private router: Router) { }
 
   ngOnInit() {
-    //console.log(this.valor);
+    
   }
 
   async menu() {
+    if (this.seccion === 'HOME') {
+      this.cssSeccion[0] = 'seccionActivaMi';
+    } else{
+      this.cssSeccion[0] = 'seccionInactivaMi';
+    }
+    if (this.seccion === 'SERVICIOS') {
+      this.cssSeccion[1] = 'seccionActivaMi';
+    } else{
+      this.cssSeccion[1] = 'seccionInactivaMi';
+    }
+    if (this.seccion === 'NOSOTROS') {
+      this.cssSeccion[2] = 'seccionActivaMi';
+    } else{
+      this.cssSeccion[2] = 'seccionInactivaMi';
+    }
+    if (this.seccion === 'CONTACTO') {
+      this.cssSeccion[3] = 'seccionActivaMi';
+    } else{
+      this.cssSeccion[3] = 'seccionInactivaMi';
+    }
+
 // El código se ejecuta de manera asíncrona
     // El código le dice espera hasta que el actionseeh controller cree el actionSheet
     const actionSheet = await this.actionSheetCtrl.create({
@@ -31,7 +53,8 @@ export class HeaderComponent implements OnInit {
         // Se ve mejor en IOs, y le da al ícono un tono rojo, de que va a romper algo.
         role: 'destructive',
         //Le agrego CSS
-        cssClass: 'rojo',
+        //cssClass: 'rojo',
+        cssClass: this.cssSeccion[0],
         // Los íconos están en la docu de Ionic
         icon: 'home',
         // Ataja el evento click
@@ -46,14 +69,6 @@ export class HeaderComponent implements OnInit {
           this.router.navigate(['/mi-servicios']);
         }
       },
-      /*
-      {
-        text: 'Portfolio',
-        icon: 'brush',
-        handler: () => {
-          //console.log('Play clicked');
-        }
-      },*/
       {
         text: 'Nosotros',
         icon: 'person',
@@ -64,7 +79,6 @@ export class HeaderComponent implements OnInit {
       {
         text: 'Contacto',
         icon: 'contact',
-        //role: 'cancel',
         handler: () => {
           this.router.navigate(['/mi-contacto']);
         }
